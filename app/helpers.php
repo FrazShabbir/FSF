0421912f1c122be0e6c3a1aa0d310862272c63c9
Fraz Shabbir <fraz.shabbir54@gmail.com>
2022-12-28 23:47:05 +0500

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -190,22 +190,23 @@ if (! function_exists('SendMessage')) {
     function SendMessage($number, $message)
     {
         try {
-            $account_sid = env("TWILIO_SID");
-            $auth_token = env("TWILIO_TOKEN");
-            $twilio_number = env("TWILIO_FROM");
+            if (env() == 'production') {
+            } else {
+                $account_sid = env("TWILIO_SID");
+                $auth_token = env("TWILIO_TOKEN");
+                $twilio_number = env("TWILIO_FROM");
 
-            if (substr($number, 0, 1) != '+') {
-                $number = '+'.$number;
-               
+                if (substr($number, 0, 1) != '+') {
+                    $number = '+'.$number;
+                }
+
+                $client = new Client($account_sid, $auth_token);
+                $client->messages->create($number, [
+                    'from' => $twilio_number,
+                    'body' => $message
+                    ]);
             }
-
-            $client = new Client($account_sid, $auth_token);
-            $client->messages->create($number, [
-                'from' => $twilio_number,
-                'body' => $message
-                ]);
         } catch (\Throwable $th) {
-            
             return $th->getMessage();
         }
     }
